b0463594c19d7b1a9f972a7b63235aaa98d54697
Patrick Raedler <code@readpato.dev>
2024-12-01 17:02:52 +0100

    Initial commit

---
+++ app/components/CtaIcon.vue
@@ -0,0 +1,24 @@
+<script setup lang="ts">
+import type { ButtonVariants } from '~/components/ui/button'
+
+const { variant = 'outline' } = defineProps<{
+  asChild?: boolean
+  icon: string
+  isAnchor?: boolean
+  to?: string
+  variant?: ButtonVariants['variant']
+}>()
+</script>
+
+<template>
+  <SCButton size="icon" :variant="variant" class="size-8" :as-child="asChild">
+    <template v-if="asChild && isAnchor">
+      <NuxtLink :to="to">
+        <Icon :name="`carbon:${icon}`" class="size-5" />
+      </NuxtLink>
+    </template>
+    <template v-else>
+      <Icon :name="`carbon:${icon}`" class="size-5" />
+    </template>
+  </SCButton>
+</template>
---
+++ nuxt.config.ts
@@ -0,0 +1,26 @@
+export default defineNuxtConfig({
+  devtools: { enabled: true },
+  future: {
+    compatibilityVersion: 4,
+  },
+  modules: [
+    '@nuxt/test-utils/module',
+    '@nuxtjs/tailwindcss',
+    '@nuxtjs/color-mode',
+    '@vueuse/nuxt',
+    'shadcn-nuxt',
+    '@nuxt/icon',
+    'nuxt-svgo',
+  ],
+  colorMode: {
+    classSuffix: '',
+  },
+  svgo: {
+    defaultImport: 'component',
+  },
+  shadcn: {
+    prefix: 'SC',
+    componentDir: './app/components/ui',
+  },
+  compatibilityDate: '2024-07-13',
+})
